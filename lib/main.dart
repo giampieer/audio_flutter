2e7ef956b09d34e836a28886e8ff708cebe0c43b
unknown <giampieer24@gmail.com>
2018-12-29 01:11:08 -0500

    add updates audio library

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+import 'package:audioplayer/audioplayer.dart';
+import 'package:flutter/material.dart';
+
+typedef void OnError(Exception exception);
+
+const kUrl = "http://104.168.176.52:8000/stream.mp3";
+const kUrl2 = "http://www.rxlabz.com/labz/audio.mp3";
+
+void main() {
+  runApp(new MaterialApp(home: new Scaffold(body: new AudioApp())));
+}
+
+enum PlayerState { stopped, playing, paused }
+
+class AudioApp extends StatefulWidget {
+  @override
+  _AudioAppState createState() => new _AudioAppState();
+}
+
+class _AudioAppState extends State<AudioApp> {
+  Duration duration;
+  Duration position;
+
+  AudioPlayer audioPlayer;
+
+  String localFilePath;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  bool isMuted = false;
+
+  StreamSubscription _positionSubscription;
+  StreamSubscription _audioPlayerStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    //initAudioPlayer();
+    play();
+  }
+
+  void initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    _positionSubscription = audioPlayer.onAudioPositionChanged
+        .listen((p) => setState(() => position = p));
+    _audioPlayerStateSubscription =
+        audioPlayer.onPlayerStateChanged.listen((s) {
+          if (s == AudioPlayerState.PLAYING) {
+            setState(() => duration = audioPlayer.duration);
+          }
+        },
+        );
+  }
+
+  Future play() async {
+    audioPlayer = new AudioPlayer();
+    await audioPlayer.play(kUrl);
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = new Duration();
+    });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Material(
+            elevation: 2.0,
+            color: Colors.grey[200],
+            child: new Center(
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    new Material(child: _buildPlayer()),
+                    localFilePath != null
+                        ? new Text(localFilePath)
+                        : new Container(),
+                  ]),
+            )));
+  }
+
+  Widget _buildPlayer() => new Container(
+      padding: new EdgeInsets.all(16.0),
+      child: new Column(mainAxisSize: MainAxisSize.min, children: [
+        new Row(mainAxisSize: MainAxisSize.min, children: [
+          new IconButton(
+              onPressed: isPlaying ? null : () => play(),
+              iconSize: 64.0,
+              icon: new Icon(Icons.play_arrow),
+              color: Colors.cyan),
+          new IconButton(
+              onPressed: isPlaying || isPaused ? () => stop() : null,
+              iconSize: 64.0,
+              icon: new Icon(Icons.stop),
+              color: Colors.cyan),
+        ]),
+      ]));
+}
